4babd19e87f2b9b14b7f6e2c5df2d9a5deec9f5f
1cbyc <ei@nsisong.com>
2025-11-24 02:07:36 +0100

    feat(src/components): Add NetworkSelector.jsx

---
+++ src/components/NetworkSelector.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { FormGroup, FormControl, Col, ControlLabel } from 'react-bootstrap';
+import { NETWORKS, getNetworkById } from '../networks';
+
+/**
+ * Network selector component
+ */
+function NetworkSelector({ selectedNetwork, onNetworkChange, validation }) {
+  const handleChange = (event) => {
+    const networkId = event.target.value;
+    const network = getNetworkById(networkId);
+    window.localStorage.setItem('selectedNetwork', networkId);
+    onNetworkChange(network);
+  };
+
+  return (
+    <FormGroup controlId="network">
+      <Col componentClass={ControlLabel} sm={2}>
+        Network
+      </Col>
+      <Col sm={10}>
+        <FormControl
+          componentClass="select"
+          value={selectedNetwork.id}
+          onChange={handleChange}
+        >
+          {Object.values(NETWORKS).map(network => (
+            <option key={network.id} value={network.id}>
+              {network.name}
+            </option>
+          ))}
+        </FormControl>
+        {validation && validation.error && (
+          <span className="text-danger" style={{ display: 'block', marginTop: '5px' }}>
+            {validation.error}
+          </span>
+        )}
+        <p className="text-muted">
+          Select the Ethereum network. API URL will be updated automatically.
+        </p>
+      </Col>
+    </FormGroup>
+  );
+}
+
+export default NetworkSelector;
+
