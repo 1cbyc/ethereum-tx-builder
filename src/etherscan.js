d548413e4c2e0c04d004e8130294d6e6d9ce74c2
1cbyc <ei@nsisong.com>
2025-11-24 02:34:16 +0100

    fix(src): Update etherscan.js

--- src/etherscan.js
+++ src/etherscan.js
@@ -1,6 +1,6 @@
 // Polyfills
-require("babel-core/register"); // http://stackoverflow.com/a/33527883/315168
-require("babel-polyfill"); // http://stackoverflow.com/a/33527883/315168
+require('babel-core/register'); // http://stackoverflow.com/a/33527883/315168
+require('babel-polyfill'); // http://stackoverflow.com/a/33527883/315168
 require('es6-promise').polyfill(); // https://github.com/matthew-andrews/isomorphic-fetch
 
 import fetch from 'isomorphic-fetch';
@@ -8,112 +8,126 @@ import Web3 from 'web3';
 
 const web3 = new Web3();
 
-/** Throw when API call failes. */
+/**
+ * Custom error class for API call failures
+ */
 export class APIError extends Error {
-};
+}
 
 
+/**
+ * Etherscan API wrapper class
+ * Provides methods to interact with Etherscan API for blockchain data
+ */
 export class API {
-
+  /**
+   * Create a new API instance
+   * @param {string} baseURL - Base URL for Etherscan API (e.g., https://api.etherscan.io/api)
+   * @param {string} apiKey - Etherscan API key
+   */
   constructor(baseURL, apiKey) {
     this.baseURL = baseURL;
-    this.apiKey= apiKey;
+    this.apiKey = apiKey;
   }
 
   /**
    * Perform an async HTTP request to EtherScan API
-   * @param params
-   * @returns {*}
+   * @param {object} params - Query parameters for the API request
+   * @returns {Promise<*>} API response result
+   * @throws {APIError} If the API returns an error
    */
   async makeRequest(params) {
-
+    // Build query string from parameters
     // http://stackoverflow.com/a/34209399/315168
-    let esc = encodeURIComponent;
-    let query = Object.keys(params)
-      .map(k => esc(k) + '=' + esc(params[k]))
+    const esc = encodeURIComponent;
+    const query = Object.keys(params)
+      .map(k => `${esc(k)}=${esc(params[k])}`)
       .join('&');
 
-    let response = await fetch(this.baseURL + "?" + query);
-    let data = await response.json();
+    const response = await fetch(`${this.baseURL}?${query}`);
+    const data = await response.json();
 
-    if(data.error) {
-      // {"jsonrpc":"2.0","error":{"code":-32010,"message":"Insufficient funds. Account you try to send transaction from does not have enough funds. Required 62914560000000000 and got: 37085440000000000.","data":null},"id":1}
+    if (data.error) {
+      // Example error: {"jsonrpc":"2.0","error":{"code":-32010,"message":"Insufficient funds..."},"id":1}
       throw new APIError(data.error.message);
     }
 
-    console.log("API result", data);
+    // eslint-disable-next-line no-console
+    console.log('API result', data);
     return data.result;
   }
 
   /**
-   * Return account balance as as Ether, a BigDecimal string.
-   * @param address
-   * @returns {*}
+   * Get account balance in ETH
+   * @param {string} address - Ethereum address to check balance for
+   * @returns {Promise<string>} Balance in ETH as a string
    */
   async getBalance(address) {
-    // https://api.etherscan.io/api?module=account&action=balance&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a&tag=latest&apikey=YourApiKeyToken
-    let params = {
+    // API endpoint: https://api.etherscan.io/api?module=account&action=balance&address=...
+    const params = {
       apikey: this.apiKey,
-      module: "account",
-      action: "balance",
-      address: address,
-      tag: "latest",
+      module: 'account',
+      action: 'balance',
+      address,
+      tag: 'latest',
     };
-    let balance = await this.makeRequest(params);
-    if(balance) {
-      return web3.fromWei(balance, "ether");
-    } else {
-      return balance;
+    const balance = await this.makeRequest(params);
+    if (balance) {
+      return web3.fromWei(balance, 'ether');
     }
+    return balance;
   }
 
   /**
    * Get sent transaction count, including transactions in memory pool.
+   * This can be used to determine the next nonce.
    *
-   * Also can be used as a nonce.
-   *
-   * @param address
+   * @param {string} address - Ethereum address
+   * @returns {Promise<string>} Transaction count as hexadecimal string
    */
   async getTransactionCount(address) {
-    let params = {
+    const params = {
       apikey: this.apiKey,
-      module: "proxy",
-      action: "eth_GetTransactionCount",
-      address: address,
-      tag: "pending",
+      module: 'proxy',
+      action: 'eth_GetTransactionCount',
+      address,
+      tag: 'pending',
     };
     return await this.makeRequest(params);
   }
 
   /**
-   * Push out a raw signed transaction
+   * Broadcast a raw signed transaction to the network
    *
-   * @param data Transaction as hex
-   * @return tx hash
+   * @param {string} data - Raw signed transaction as hexadecimal string
+   * @returns {Promise<string>} Transaction hash
+   * @throws {Error} If transaction data is invalid
    */
   async sendRaw(data) {
-
-    if(!data.startsWith("0x")) {
-      throw new Error("Data does not look like 0x hex string:" + data);
+    if (!data.startsWith('0x')) {
+      throw new Error(`Data does not look like 0x hex string: ${data}`);
     }
 
-    let params = {
+    const params = {
       apikey: this.apiKey,
-      module: "proxy",
-      action: "eth_sendRawTransaction",
+      module: 'proxy',
+      action: 'eth_sendRawTransaction',
       hex: data,
-      tag: "latest",
+      tag: 'latest',
     };
     return await this.makeRequest(params);
   }
 
+  /**
+   * Get current gas price from the network
+   * @returns {Promise<string>} Gas price in wei as hexadecimal string
+   */
   async getGasPrice() {
-
-    let params = {
+    const params = {
       apikey: this.apiKey,
-      module: "proxy",
-      action: "eth_gasPrice",
+      module: 'proxy',
+      action: 'eth_gasPrice',
     };
     return await this.makeRequest(params);
   }
-};
+}
